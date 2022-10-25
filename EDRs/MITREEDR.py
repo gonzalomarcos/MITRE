ea4e433523d57eaace0fa6cafaee936c282c090f
Gonzalo <gonzalo.marcos@hotmail.es>
2022-10-25 18:08:53 +0200

    Update MITREEDR.py

--- EDRs/MITREEDR.py
+++ EDRs/MITREEDR.py
@@ -19,7 +19,7 @@ itemsPrueba ={}
 
 
 def proper_round(num, dec=0):
-    num = num*4
+    num = num*5
     num = str(num)[:str(num).index('.')+dec+2]
     if num[-1]>='5':
         return float(num[:-2-(not dec)]+str(int(num[-2-(not dec)])+1))
